cfb10e60a0e9b2c70448f086107b8eb790160db8
Peter St. John <peterc.stjohn@gmail.com>
2022-01-30 06:38:37 -0700

    adding new model

---
+++ 20220128_icsd_and_battery/train_model.py
@@ -0,0 +1,187 @@
+import math
+import os
+import shutil
+from collections import Counter
+from pathlib import Path
+
+import nfp
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import tensorflow_addons as tfa
+from nfp.layers import RBFExpansion
+from nfp.preprocessing.crystal_preprocessor import PymatgenPreprocessor
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers
+from tqdm.auto import tqdm
+
+tqdm.pandas()
+
+inputs_dir = Path("/projects/rlmolecule/pstjohn/crystal_inputs/")
+
+data = pd.read_pickle(Path(inputs_dir, "20211227_all_data.p"))
+max_distance = data['inputs'].apply(lambda x: x['distance'].max())
+data = data[max_distance < 100]  # drop some problematic icsd structures
+
+preprocessor = PymatgenPreprocessor()
+preprocessor.from_json(Path(inputs_dir, "20211227_preprocessor.json"))
+
+train, valid = train_test_split(data, test_size=2000, random_state=1)
+valid, test = train_test_split(valid, test_size=0.5, random_state=2)
+
+
+def calculate_output_bias(train):
+    """ We can get a reasonable guess for the output bias by just assuming the crystal's
+     energy is a linear sum over it's element types """
+    # This just converts to a count of each element by crystal
+    site_counts = train.inputs.progress_apply(
+        lambda x: pd.Series(Counter(x["site"]))
+    ).fillna(0)
+    # Linear regression assumes a sum, while we average over sites in the neural network.
+    # Here, we make the regression target the total energy, not the site-averaged energy
+    num_sites = site_counts.sum(1)
+    total_energies = train["energyperatom"] * num_sites
+
+    # Do the least-squares regression, and stack on zeros for the mask and unknown tokens
+    output_bias = np.linalg.lstsq(site_counts, total_energies, rcond=None)[0]
+    output_bias = np.hstack([np.zeros(2), output_bias])
+    return output_bias
+
+
+def build_dataset(split, batch_size):
+    return (
+        tf.data.Dataset.from_generator(
+            lambda: ((row.inputs, row.energyperatom) for _, row in split.iterrows()),
+            output_signature=(
+                preprocessor.output_signature,
+                tf.TensorSpec((), dtype=tf.float32),
+            ),
+        )
+        .cache()
+        .shuffle(buffer_size=len(split))
+        .padded_batch(
+            batch_size=batch_size,
+            padding_values=(
+                preprocessor.padding_values,
+                tf.constant(np.nan, dtype=tf.float32),
+            ),
+        )
+        .prefetch(tf.data.experimental.AUTOTUNE)
+    )
+
+
+# Calculate an initial guess for the output bias
+output_bias = calculate_output_bias(train)
+
+batch_size = 64
+train_dataset = build_dataset(train, batch_size=batch_size)
+valid_dataset = build_dataset(valid, batch_size=batch_size)
+
+
+# Keras model
+site_class = layers.Input(shape=[None], dtype=tf.int64, name="site")
+distances = layers.Input(shape=[None], dtype=tf.float32, name="distance")
+connectivity = layers.Input(shape=[None, 2], dtype=tf.int64, name="connectivity")
+input_tensors = [site_class, distances, connectivity]
+
+embed_dimension = 256
+num_messages = 6
+
+atom_state = layers.Embedding(
+    preprocessor.site_classes, embed_dimension, name="site_embedding", mask_zero=True
+)(site_class)
+
+atom_mean = layers.Embedding(
+    preprocessor.site_classes,
+    1,
+    name="site_mean",
+    mask_zero=True,
+    embeddings_initializer=tf.keras.initializers.Constant(output_bias),
+)(site_class)
+
+rbf_distance = RBFExpansion(
+    dimension=128, init_max_distance=7, init_gap=30, trainable=True
+)(distances)
+
+bond_state = layers.Dense(embed_dimension)(rbf_distance)
+
+for _ in range(num_messages):
+    new_bond_state = nfp.EdgeUpdate()([atom_state, bond_state, connectivity])
+    bond_state = layers.Add()([bond_state, new_bond_state])
+    new_atom_state = nfp.NodeUpdate()([atom_state, bond_state, connectivity])
+    atom_state = layers.Add()([atom_state, new_atom_state])
+
+# Reduce the atom state vector to a single energy prediction
+atom_state = layers.Dense(
+    1,
+    name="site_energy_offset",
+    kernel_initializer=tf.keras.initializers.RandomNormal(
+        mean=0.0, stddev=1e-6, seed=None
+    ),
+)(atom_state)
+
+# Add this 'offset' prediction to the learned mean energy for the given element type
+atom_state = layers.Add(name="add_energy_offset")([atom_state, atom_mean])
+
+# Calculate a final mean energy per atom
+out = tf.keras.layers.GlobalAveragePooling1D()(atom_state)
+
+model = tf.keras.Model(input_tensors, [out])
+
+
+# Train the model
+STEPS_PER_EPOCH = math.ceil(len(train) / batch_size)  # number of training examples
+lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+    1e-4, decay_steps=STEPS_PER_EPOCH * 50, decay_rate=1, staircase=False
+)
+
+wd_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+    1e-5, decay_steps=STEPS_PER_EPOCH * 50, decay_rate=1, staircase=False
+)
+
+optimizer = tfa.optimizers.AdamW(
+    learning_rate=lr_schedule, weight_decay=wd_schedule, global_clipnorm=1.0
+)
+
+model.compile(loss="mae", optimizer=optimizer)
+
+model_name = "20220128_icsd_and_battery"
+
+if not os.path.exists(model_name):
+    os.makedirs(model_name)
+
+# Make a backup of the job submission script
+shutil.copy(__file__, model_name)
+
+filepath = model_name + "/best_model.hdf5"
+checkpoint = tf.keras.callbacks.ModelCheckpoint(
+    filepath, save_best_only=True, verbose=0
+)
+csv_logger = tf.keras.callbacks.CSVLogger(model_name + "/log.csv")
+
+if __name__ == "__main__":
+    model.fit(
+        train_dataset,
+        validation_data=valid_dataset,
+        epochs=100,
+        callbacks=[checkpoint, csv_logger],
+        verbose=1,
+    )
+    
+    data['set'] = 'train'
+    data.loc[data.index.isin(valid.index), 'set'] = 'valid'
+    data.loc[data.index.isin(test.index), 'set'] = 'test'
+    
+    dataset = (
+        tf.data.Dataset.from_generator(
+            lambda: (row.inputs for _, row in data.iterrows()),
+            output_signature=preprocessor.output_signature)
+        .padded_batch(batch_size=128,
+                      padding_values=preprocessor.padding_values)
+        .prefetch(tf.data.experimental.AUTOTUNE)
+    )
+
+    predictions = model.predict(dataset, verbose=1)
+
+    data['energy_predicted'] = predictions
+    data.drop('inputs', axis=1).to_csv(model_name + '/predicted_energies.csv.gz', compression='gzip')
